8c7f68827985bc82d4ecc98563d690cb97edf42b
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-18 23:10:40 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Display("Hello, World!");
+        Display("Welcome to learning C#.");
+    }
+
+    // Funkcja Display
+    static void Display(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
